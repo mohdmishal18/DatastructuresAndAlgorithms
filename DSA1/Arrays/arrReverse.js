53ae71a763691336caf53ffd3c89c32b83fbfbb6
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-27 11:18:15 +0530

    solved an arrya question

---
+++ DSA1/Arrays/arrReverse.js
@@ -0,0 +1,18 @@
+// First Approach $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// let arr = [7,5,8,0,6,2,0,9,3,4,0]
+
+// for(let left = 0,right = arr.length - 1; left < arr.length/2,right >= arr.length/2; left++,right--)
+// {
+//     let temp = 0;
+//     temp = arr[left];
+//     arr[left] = arr[right] ;
+//     arr[right] = temp;
+// }
+
+// console.log(arr);
+
+// Second Approach $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// let arr = [7,5,8,0,6,2,0,9,3,4,0]
+
