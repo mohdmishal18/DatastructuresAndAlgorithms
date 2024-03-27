cea7ccdae59eb58ed4c9ae40d3048ed668e2b8b2
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-27 11:56:24 +0530

    2nd login for reversing an array

--- DSA1/Arrays/arrReverse.js
+++ DSA1/Arrays/arrReverse.js
@@ -1,3 +1,4 @@
+//1.
 // First Approach $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 // let arr = [7,5,8,0,6,2,0,9,3,4,0]
@@ -12,7 +13,18 @@
 
 // console.log(arr);
 
+//2.
 // Second Approach $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-// let arr = [7,5,8,0,6,2,0,9,3,4,0]
+// let arr = [1,2,3,4,5]
+
+// for(let index = 0; index < arr.length/2; index++)
+// {
+//     let temp = 0; 
+//     temp = arr[index];
+//     arr[index] = arr[(arr.length - 1) - index] ;
+//     arr[(arr.length - 1) - index] = temp;
+// }
+
+// console.log(arr);
 
