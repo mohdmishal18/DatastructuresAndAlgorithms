95ebd4a0f4b5ed6a0bb798676f7063d29f974bf3
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-28 10:45:04 +0530

    algorithm

---
+++ DSA1/algorithms/fibonacciSeq.js
@@ -0,0 +1,23 @@
+// Problem = Given a number "n",find the first 'n' elements of the fibonacci sequence.
+
+                                                                            //==========================================================
+                                                                            //Fibonacci Sequence : It is a sequence in which each Number
+                                                                            //is the sum of two preceeding ones.
+                                                                            //==========================================================
+                                                                            //note : The first 2 number in the sequence are 0 and 1;
+                                                                            //fibonacciSequence(2) = [0,1]
+                                                                            //fibonacciSequence(3) = [0,1,1]
+                                                                            //fibonacciSequence(7)= [0,1,1,2,3,5,8]
+function fibonacciSequence(n)
+{
+    const fib = [0,1];
+
+    for(let i = 2; i < n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
+}
+
+console.log(fibonacciSequence(7));
