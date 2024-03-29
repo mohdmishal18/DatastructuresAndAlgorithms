22c6f157df67a436151a1491f36523fcc1e734c5
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-29 17:24:45 +0530

    changes

---
+++ DSA1/2 linkedLists/LinkedLists.js
@@ -0,0 +1,31 @@
+class Node
+{
+    constructor(value)
+    {
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList
+{
+    constructor()
+    {
+        this.head = null
+        this.size = 0
+    }
+
+    isEmpty()
+    {
+        return this.size === 0;
+    }
+
+    getSize()
+    {
+        return this.size
+    }
+}
+
+const list = new LinkedList()
+console.log("List is empty ? :" ,list.isEmpty());
+console.log("list size : ",list.getSize());
