a09dcd55552e5645147489910c948ed959b2552c
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-29 15:27:21 +0530

    added some array properties

---
+++ DSA1/Arrays/array.js
@@ -0,0 +1,13 @@
+const arr = [1,3,4,"mishal",87];
+
+arr.push(3); // add element to the end of the array
+arr.unshift(9,5); // add element to the start of the array
+arr.pop() // remove an element from the end of the array
+arr.shift() // remove an element from the start of an array
+
+for(const item of arr)console.log(item);
+
+// Other methods 
+// map, filter ,reduce , concat, slice and splice.
+
+    
