527bee57e9c7b6e9e1505decc858c084b0afc02f
Mohammed Mishal <mohdmishal18@gmail.com>
2024-04-07 17:00:39 +0530

    quick sort done

--- DSA2/1 Sorting/quickSort.js
+++ DSA2/1 Sorting/quickSort.js
@@ -2,7 +2,26 @@
 
 function quickSort(arr)
 {
-    
+    if(arr.length < 2)return arr
+
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+
+    for(let i = 0; i < arr.length - 1; i++)
+    {
+        if(arr[i] <= pivot)left.push(arr[i])
+        else right.push(arr[i])
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
 let arr = [7,9,1,3,4,5,2,0,9,3,5,2,8,7]
+
+console.log(quickSort(arr))
+
+// Big-O
+// worst case = O(n ^ 2)
+// average case = O(n log n)
+
