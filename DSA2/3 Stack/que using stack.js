c3f14c985dc282da5b741855e20b6a2a96f4e70e
Mohammed Mishal <mohdmishal18@gmail.com>
2024-04-11 20:17:12 +0530

    implemented queue using stackss

--- DSA2/3 Stack/que using stack.js
+++ DSA2/3 Stack/que using stack.js
@@ -1,2 +1,59 @@
 // implement queue using two stacks.
 
+class Queue
+{
+    constructor()
+    {
+        this.L1 = []
+        this.L2 = []
+    }
+
+    enqueue(data)
+    {
+        while(this.L1.length !== 0)
+        {
+            this.L2.push(this.L1.pop())
+        }
+
+        this.L1.push(data)
+
+        while(this.L2.length !== 0)
+        {
+            this.L1.push(this.L2.pop())
+        }
+    }
+
+    dequeue()
+    {
+        if(this.L1.length === 0)return "the queue is empty"
+        else
+        {
+            return this.L1.pop()
+        }
+    }
+
+    print()
+    {
+        if(this.L1.length === 0)return console.log("the queue is empty!!")
+        else
+        {
+            let queue = ""
+            for(let i = 0; i < this.L1.length; i++)
+            {
+                queue += this.L1[i] + " "
+            }
+            return console.log("queue : " + queue);
+        }
+    }
+}
+
+const queue = new Queue()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.print()
+
+queue.dequeue()
+queue.print()
