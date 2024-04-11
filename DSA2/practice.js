3328e72748236f84fc1c892d2a26e48e5bb167bc
Mohammed Mishal <mohdmishal18@gmail.com>
2024-04-11 15:18:19 +0530

    practiced

--- DSA2/practice.js
+++ DSA2/practice.js
@@ -1,29 +1,26 @@
-
-// practice merge sort
+// merge sort
 
 function mergeSort(arr)
 {
     if(arr.length < 2)return arr
-
     let mid = Math.floor(arr.length / 2)
-    let left = arr.slice(0 , mid)
-    let right = arr.slice(mid)
+    let leftArr = arr.slice(0 , mid)
+    let rightArr = arr.slice(mid)
 
-    return merge(mergeSort(left), mergeSort(right))
+    return merge(mergeSort(leftArr),mergeSort(rightArr))
 }
 
 function merge(leftArr , rightArr)
 {
-    let sortedArr = []
-
+    let sorted = []
     while(leftArr.length && rightArr.length)
     {
-        if(leftArr[0] <= rightArr[0])sortedArr.push(leftArr.shift())
-        else sortedArr.push(rightArr.shift())
+        if(leftArr[0] < rightArr[0])sorted.push(leftArr.shift())
+        else sorted.push(rightArr.shift())
     }
 
-    return [...sortedArr , ...leftArr , ...rightArr]
+    return [...sorted , ... leftArr , ...rightArr]
 }
 
-let arr = [-900,4,1,6,7,434,2,4,677,1,-888]
+let arr = [9,8,7,3,5,2,9,8,4,6,5,4,1,0]
 console.log(mergeSort(arr))
