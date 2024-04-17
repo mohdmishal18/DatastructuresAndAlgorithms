a051458d44e35fca3fdd4438c386f614b2200ed4
Mohammed Mishal <mohdmishal18@gmail.com>
2024-04-17 12:28:45 +0530

    practiced

---
+++ DSA3/3 Heap/maxHeap.js
@@ -0,0 +1,23 @@
+//Max heap
+// in this the value of parent node is greater than the child nodes
+
+class maxHeap
+{
+    constructor()
+    {
+        this.heap = []
+        this.size = 0
+    }
+
+    isEmpty()
+    {
+        return this.size === 0
+    }
+
+    getSize()
+    {
+        return this.size
+    }
+
+    
+}
---
+++ DSA3/3 Heap/minHeap.js
@@ -0,0 +1,3 @@
+// Min head
+// in min heap , the values of parent nodes are smaller than hte child node
+
